491718997604939fa847094ebab52f157105aab0
Valentina Restrepo <vrestrepomontoya@gmail.com>
2023-08-16 21:39:21 -0500

    Cambios en TipoSubcategoria

---
+++ src/main/java/co/com/ppi/Street/models/entity/DetalleCompraEntity.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package co.com.ppi.Street.models.entity;
+
+/**
+ * TODO: descripción <br>
+ * Creado el Aug 16, 2023 a las 9:37:16 PM <br>
+ *
+ */
+public class DetalleCompraEntity {
+
+}
